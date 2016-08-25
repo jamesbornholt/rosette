6f104b00786bea55cf8958e36e7d317f1bf97b48
Emina Torlak <emina@alum.mit.edu>
2016-08-25 11:22:21 -0700

    Add list microbenchmarks.

---
+++ test/profile/micro/README.md
@@ -0,0 +1,6 @@
+# Microbenchmarks
+
+Contains the following microbenchmarks for symbolic profiling:     
+
+- `list.rkt` includes slow and fast variants of various list procedures, both artificial and lifted from real examples.
+- `tree.rkt` includes slow and fast variants of tree procedures.
---
+++ test/profile/micro/list.rkt
@@ -0,0 +1,62 @@
+#lang rosette
+
+; Micro-benchmarks for various list operations.
+
+(require "../bench.rkt")
+(provide (all-defined-out))
+
+; Returns a new list that updates lst[pos] with val.
+; Worst-case behavior trigerred when given a symbolic list and position.
+(bench-define (list-set lst pos val)
+  (bench
+   (let-values ([(front back) (split-at lst pos)])
+     (append front (cons val (cdr back))))
+   (for/all ([lst lst]) 
+    (map (lambda (i v) (if (= pos i) val v))
+         (build-list (length lst) identity)
+         lst))))
+
+; Returns a new list of the form lst[0..pos) ++ lst(pos..).
+; Worst-case behavior trigerred when given a symbolic list and position.
+(bench-define (remove-at lst pos)
+  (bench
+   (let-values ([(front back) (split-at lst pos)])
+     (append front (rest back)))
+   (for/all ([lst lst])
+    (let loop ([i 0] [front empty] [back lst])
+      (if (= i pos)
+          (append front (cdr back))
+          (loop (add1 i) (append front (list (car back))) (cdr back)))))))
+
+; Returns xs ++ ys if xs and ys have the same length;
+; othwerwise returns xs ++ xs.  Worst-case behavior
+; trigerred when given two symbolic lists.
+(bench-define (append-if xs ys)
+  (bench
+   (if (= (length xs) (length ys))
+       (append xs ys)
+       (append xs xs))
+   (for*/all ([xs xs][ys ys])  
+     (if (= (length xs) (length ys))
+         (append xs ys)
+         (append xs xs)))))
+
+; Like lst-set, returns a new list that updates lst[pos] with val.
+; Unlike lst-set, this shows the difference between having multiple
+; and a single recursive call. Worst-case behavior trigerred when
+; given a concrete list and a symbolic position.  In particular,
+; both the slow and fast version produce a concrete list of size N,
+; but the symbolic value's at the ith position are of size O(i) in
+; the slow case and O(1) in the fast case.
+(bench-define (update lst pos val)
+  (bench
+   (match lst
+     [(list) lst]
+     [(list x xs ...)
+      (if (= pos 0) 
+          (cons val xs)
+          (cons x (update lst (- pos 1) val)))])
+   (match lst
+     [(list) lst]
+     [(list x xs ...)
+      (cons (if (= pos 0) val x) (update lst (- pos 1) val))])))
