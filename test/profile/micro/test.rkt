19a142943867f07e973e3224028312a4ad55d990
Emina Torlak <emina@alum.mit.edu>
2016-08-29 19:47:17 -0700

    Simple imports work ...

--- test/profile/micro/test.rkt
+++ test/profile/micro/test.rkt
@@ -1,13 +1,18 @@
 #lang rosette
 
-(require "hide.rkt" (only-in rosette/lib/profile profile-thunk))
+(require "hide.rkt" "../bench.rkt" (only-in rosette/lib/profile profile-thunk))
 
 
 
 (define (append-if xs ys)
+  (bench
    (if (= (length xs) (length ys))
        (hide xs ys)
-       (hide xs xs)))
+       (hide xs xs))
+    (for*/all ([xs xs][ys ys])  
+     (if (= (length xs) (length ys))
+         (append xs ys)
+         (append xs xs)))))
 
 (define-symbolic b boolean?)
 
