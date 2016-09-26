f99c816a45c9083de2a82d004a74282702335806
James Bornholt <bornholt@cs.washington.edu>
2016-09-26 10:58:48 -0700

    Implement Matthew's suggested changes to profiler instrumentation

--- rosette/lib/profile/compile.rkt
+++ rosette/lib/profile/compile.rkt
@@ -1,5 +1,6 @@
 #lang racket/base
 
+(require syntax/strip-context)
 (provide symbolic-profile-compile-handler)
 
 (define (make-symbolic-profile-compile-handler)
@@ -22,22 +23,15 @@
   (syntax-case stx ()
     [(mod id lang (mod-begin forms ...))
      (and (id=? #'mod 'module) (id=? #'lang 'rosette) (id=? #'mod-begin '#%module-begin))
-     (with-syntax ([body (strip-context #`(mod-begin (require rosette/lib/profile/instrument)
-                                                     forms ...))])
-       (printf "INSTRUMENTING ~a\n" #'id)
-       (quasisyntax/loc stx
-         (mod id lang body)))] 
+     ; Create a fresh scope to ensure that the inserted `require` is bound to
+     ; this file's `require` rather than one from the instrumented module
+     (let ([introduce (make-syntax-introducer #t)])  ; create a fresh scope
+       (with-syntax ([body #`(mod-begin
+                              (#,(introduce #'require)  ; add fresh scope to cancel below
+                               #,(replace-context #'lang #'rosette/lib/profile/instrument))
+                              forms ...)])
+         (printf "INSTRUMENTING ~a\n" #'id)
+         (introduce  ; add fresh scope to the module
+          (quasisyntax/loc stx
+            (mod id lang body)))))]
     [_ stx]))
-
-(define (strip-context stx)
-  (syntax-case stx ()
-    [(e0 e ...)
-     (datum->syntax #f `(,(strip-context #'e0) ,@(map strip-context (syntax->list #'(e ...)))) stx)]
-    [e
-     (identifier? #'e)
-     (datum->syntax #f (syntax-e #'e) #'e)]
-    [_ stx]))
-
-
-
-
