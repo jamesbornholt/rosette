c4263ae4a2c33b032ae16b4ffaab178ed82c9c65
AlexKnauth <alexander@knauth.org>
2017-02-20 14:07:39 -0800

    add some feasible sleep as well

--- test/profile/micro/infeasible-sleep.rkt
+++ test/profile/micro/infeasible-sleep.rkt
@@ -4,11 +4,14 @@
   (if (< x 2)
       (if (< 2 x)
           (let ()
-            (sleep 1)
+            (sleep 1) ; infeasible sleep
             1)
-          2)
+          (let ()
+            (sleep 1) ; feasible sleep
+            2))
       3))
 
 (define-symbolic x integer?)
 
+;; There should be only ~1 second of infeasible time, not ~2.
 (main x)
